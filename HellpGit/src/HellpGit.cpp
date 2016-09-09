6ebc3d3cf92717b2d8f37686ab29f0c38333df38
Christopher KIM <chkim0676@gmail.com>
2016-09-09 17:16:07 -0500

    Add line for third .

--- HellpGit/src/HellpGit.cpp
+++ HellpGit/src/HellpGit.cpp
@@ -12,9 +12,6 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "This is second line"<<endl;
-
-
-
-
+	cout << "This is Third line"<<endl; //wow..
 	return 0;
 }
